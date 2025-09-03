70c3be08a508bb3779f4b83d1fc25bb127a0de80
yanlong.wu <wu627143829@163.com>
2025-09-03 13:01:53 +0800

    Delete invalid testcases.

--- testcases/function/api/mooncake/ha/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from testcases.mooncake.function.ha.gen_data import mooncake
-
-
-@pytest.fixture(autouse=True)
-def fixture_start_server():
-    print(mooncake)
--- testcases/ui/test_ui_demo.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-
-def test_ui_demo():
-    assert 1 + 1 == 2
